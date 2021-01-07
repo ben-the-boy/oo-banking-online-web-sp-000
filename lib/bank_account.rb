5f1bd2312816e1d5c7f0bdb3c632450be646ee51
Ben Hayes <b.t.hayes@comcast.net>
2021-01-07 23:13:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class BankAccount
   
   attr_accessor :name, :balance, :status
   
   def initialize(name)
     @name = name
+    binding.pry
     @balance = 1000
     @status = "open"
   end 
