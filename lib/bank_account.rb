2ec21422c492c58acb5d651104143d57cfcd4397
Ben Hayes <b.t.hayes@comcast.net>
2021-01-07 23:10:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,8 +3,7 @@ class BankAccount
   attr_accessor :name, :balance, :status
   
   def initialize(name)
-    @name = name
-    @name 
+    self.name = name
     @balance = 1000
     @status = "open"
   end 
