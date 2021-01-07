80713c3d232f9e79d7062ccebc79220eca0da7fc
Ben Hayes <b.t.hayes@comcast.net>
2021-01-07 23:31:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,8 +14,10 @@ class Transfer
   end
   
   def execute_transaction
-    @sender.balance = @sender.balance - @amount
-    @receiver.balance = @receiver.balance + @amount
+    if @sender.balance > @amount
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
+      @status = "complete"
   end 
   
 end
